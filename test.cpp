b51edb13691a24e7692ffc4fa9e8cc4c6c673205
Michael Korobkov <korobkov200364@gmail.com>
2021-05-08 15:49:28 +0300

    Update test.cpp

--- test.cpp
+++ test.cpp
@@ -54,7 +54,31 @@ void test_max()
     std::cout << "time singlethread: " << ms2 << " seconds" << std::endl;
 }
 
+void test_sort()
+{
+    constexpr int v_size = 1e9;
+    std::srand(std::time(0));
+    PVector<char> v(v_size);
+    for (int i = 0; i < v.size(); ++i)
+         v[i] = std::rand() % 100;
+    auto start = std::chrono::high_resolution_clock::now();
+    v.sort();
+    double ms_par = std::chrono::duration_cast<std::chrono::milliseconds>(std::chrono::high_resolution_clock::now() - start).count() / 1000.0;
+    if (!std::is_sorted(v.begin(), v.end()))
+        std::cout << "Something wrong with sorting algorithm" << std::endl;
+    std::cout << "Time multithread: " << ms_par << std::endl;
+    std::srand(std::time(0));
+    for (int i = 0; i < v.size(); ++i)
+        v[i] = std::rand() % 100;
+    start = std::chrono::high_resolution_clock::now();
+    std::sort(v.begin(), v.end());
+    double ms_single = std::chrono::duration_cast<std::chrono::milliseconds>(std::chrono::high_resolution_clock::now() - start).count() / 1000.0;
+    if (!std::is_sorted(v.begin(), v.end()))
+        std::cout << "Something wrong with std::sort" << std::endl;
+    std::cout << "Time singlethread: " << ms_single << std::endl;
+}
+
 int main()
 {
-    test_max();
+    test_sort();
 }
